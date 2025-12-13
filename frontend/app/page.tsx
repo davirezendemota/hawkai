f59af13c475234f7e16b183ebde0dcca0860d629
Davi Rezende <davi@rmconsult.io>
2025-12-13 17:47:41 -0300

    feat: update main page to use new dashboard layout

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+    </>
+  );
+}
+
